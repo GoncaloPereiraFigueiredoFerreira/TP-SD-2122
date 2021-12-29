817fe9a7197d3a1dd6e33790a7177d97d765cb55
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2021-12-29 15:14:13 +0000

    Update

--- src/Desmultiplexer/Operacoes/OperacaoI.java
+++ src/Desmultiplexer/Operacoes/OperacaoI.java
@@ -1,8 +1,9 @@
 package Desmultiplexer.Operacoes;
 
+import DataLayer.GestorDeDados;
 import Desmultiplexer.ConnectionPlusByteArray;
 import Desmultiplexer.TaggedConnection;
 
 public interface OperacaoI extends Runnable{
-    public void newRun(ConnectionPlusByteArray cpba);
+    public void newRun(ConnectionPlusByteArray cpba, GestorDeDados gestorDeDados);
 }
