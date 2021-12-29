69792f0aa281208c3f4e52496c6ac4d524f6ab23
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2021-12-29 14:45:19 +0000

    Updated cliente

--- src/Desmultiplexer/Cliente.java
+++ src/Desmultiplexer/Cliente.java
@@ -2,108 +2,49 @@ package Desmultiplexer;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Cliente {
-    private static TaggedConnection tc;
-    private Socket s;
-
+    private TaggedConnection connect() throws IOException {
+        Socket s = new Socket("localhost", 8888);
+        return new TaggedConnection(s);
+    }
     //Deviamos ter operaçao para saber quais as suas reservas
     //Deviamos retornar a data na reserva
 
-    public static class Thread1 extends Thread{
-        String user;
-        String pass;
+    public boolean criaConta(String username,String password) { //TODO:: THREAD DE CRIAR CONTA | tag 0
+        try {
+            TaggedConnection tc= connect();
+            // Envia uma password e um username
+            tc.send(0, (username).getBytes(StandardCharsets.UTF_8));
+            tc.send(0, (password).getBytes(StandardCharsets.UTF_8));
 
-        public Thread1 (String username,String password){
-            this.user = username;
-            this.pass = password;
-        }
-        @Override
-        public void run(){
-            // TODO:: THREAD DE CRIAR CONTA
-            //Não sei qual o formato da mensagem
-            String msg = user+pass;
-            try {
-                // Envia uma password e um username
-                tc.send(1,(msg).getBytes(StandardCharsets.UTF_8));
-                Thread.sleep(100);
-
-                // Recebe uma confirmação de criação de conta
-                Frame frame = tc.receive();
-                System.out.println("(1) Reply: " + new String(frame.getData()));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+            // Recebe uma confirmação de criação de conta
+            Frame frame = tc.receive();
+            return frame.getData()[0] == (byte) 0;  //0 significa que a conta foi criada, -1 caso contrário
+        } catch (Exception e) {
+            return false;
         }
     }
 
-    public static class Thread2 extends Thread{
-        String user;
-        String pass;
+    public boolean addVoo(String origem,String destino,int capacidade) { //TODO:: THREAD DE Adicionar Voo | tag 1
+        try {
+            TaggedConnection tc= connect();
+            // Envia uma origem destino e capacidade
+            tc.send(1, (origem).getBytes(StandardCharsets.UTF_8));  //Enviar Origem
+            tc.send(1, (destino).getBytes(StandardCharsets.UTF_8)); //Enviar destino
+            tc.send(1, ByteBuffer.allocate(4).putInt(capacidade).array()); //Enviar capacidade
 
-        public Thread2 (String username,String password){
-            this.user = username;
-            this.pass = password;
-        }
-        @Override
-        public void run(){
-            // TODO:: THREAD DE CRIAR CONTA
-            // Não sei qual o formato da mensagem
-            String msg = user+pass;
-            try {
-                // Envia uma password e um username
-                tc.send(2,(msg).getBytes(StandardCharsets.UTF_8));
-                Thread.sleep(100);
-
-                // Recebe uma confirmação de login
-                Frame frame = tc.receive();
-                System.out.println("(1) Reply: " + new String(frame.getData()));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+            // Recebe uma confirmação da adição
+            Frame frame = tc.receive();
+            return frame.getData()[0] == (byte) 0;  //0 significa que o voo foi criado, -1 caso contrário
+        } catch (Exception e) {
+            return false;
         }
     }
 
-    public static class Thread3 extends Thread{
-        String origem;
-        String destino;
-        int capacidade;
-        public Thread3 (String origem,String destino,int capacidade){
-            this.origem = origem;
-            this.destino = destino;
-            this.capacidade=capacidade;
-        }
-        @Override
-        public void run(){
-            // TODO:: THREAD DE ADD VOO
-            // Não sei qual o formato da mensagem
-            String msg = "ola123";
-            try {
-                // Envia uma origem destino e capacidade
-                tc.send(3,(msg).getBytes(StandardCharsets.UTF_8));
-                Thread.sleep(100);
-
-                // Recebe uma confirmação da adição
-                Frame frame = tc.receive();
-                System.out.println("(1) Reply: " + new String(frame.getData()));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-
+/*
     public static class Thread4 extends Thread{
         LocalDate date;
 
@@ -130,7 +71,7 @@ public class Cliente {
         }
     }
 
-    public static class Thread5 extends Thread{
+    public static class Thread5 extends Thread{ //Unica thread necessária
         String origem;
         List<String> destinos;
         LocalDate dInf;
@@ -249,89 +190,6 @@ public class Cliente {
             }
         }
     }
-
-   public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-
-       while(true){
-           int option = sc.nextInt();
-           Thread t=null;
-           boolean flag=false;
-           try {
-               Socket s = new Socket("localhost",9000);
-               tc = new TaggedConnection(s);
-
-           } catch (IOException e) { e.printStackTrace(); }
-
-           //Need to know the opcode
-           switch (option){
-               case 1:
-                   MenuInput m1 = new MenuInput("Insira o seu username:","Username:");
-                   MenuInput m2 = new MenuInput("Insira o seu password:","Password:");
-                   m1.executa();
-                   m2.executa();
-                   t = new Thread1(m1.getOpcao(),m2.getOpcao());
-                   break;
-               case 2:
-                   MenuInput m3 = new MenuInput("Insira o seu username:","Username:");
-                   MenuInput m4= new MenuInput("Insira o seu password:","Password:");
-                   m3.executa();
-                   m4.executa();
-                   t = new Thread2(m3.getOpcao(),m4.getOpcao());
-                   break;
-               case 3:
-                   MenuInput m5 = new MenuInput("Insira a origem:","Origem:");
-                   MenuInput m6= new MenuInput("Insira o destino:","Destino:");
-                   MenuInput m7= new MenuInput("Insira a capacidade:","Capacidade:");
-                   m5.executa();
-                   m6.executa();
-                   boolean dummyflag;
-                   int n = 0;
-                   do{
-                       dummyflag = false;
-                       m7.executa();
-                       try{
-                           n = Integer.parseInt(m7.getOpcao());
-
-                       }catch (NumberFormatException e){
-                           dummyflag = true;
-                       }
-                   }while(dummyflag);
-
-                   t= new Thread3(m5.getOpcao(),m6.getOpcao(),n);
-                   break;
-               case 4:
-                   //Menu de datas
-                   LocalDate l = LocalDate.now();
-                   t = new Thread4(l);
-                   break;
-               case 5:
-                   //Menus de origem, destino, e data inicial e final
-                    String origem = "";
-                    List<String> dest= new ArrayList<>();
-                    LocalDate inDate= LocalDate.now(), finDate = LocalDate.now();
-
-                   t= new Thread5(origem,dest,inDate,finDate);
-                   break;
-               case 6:
-                   t = new Thread6();
-                   break;
-               case 7:
-                   t= new Thread7();
-                   break;
-               case 8:
-                   MenuInput m8 = new MenuInput("Insira o codigo de reserva:","Cod Reserva:");
-                   t= new Thread8(m8.getOpcao());
-                   break;
-               case 9:
-                   t = new Thread9();
-                   break;
-                default:
-                    flag=true;
-                    break;
-            }
-            if (!flag) t.start();
-        }
-    }
+    */
 }
 
---
+++ src/Desmultiplexer/ClienteTester.java
@@ -0,0 +1,86 @@
+package Desmultiplexer;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClienteTester {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int option = sc.nextInt();
+            Thread t = null;
+            boolean flag = false;
+
+            //Need to know the opcode
+            switch (option) {
+                case 1:
+                    MenuInput m1 = new MenuInput("Insira o seu username:", "Username:");
+                    MenuInput m2 = new MenuInput("Insira o seu password:", "Password:");
+                    m1.executa();
+                    m2.executa();
+                    cliente.criaConta(m1.getOpcao(), m2.getOpcao());
+                    break;
+                case 2:
+                    MenuInput m5 = new MenuInput("Insira a origem:", "Origem:");
+                    MenuInput m6 = new MenuInput("Insira o destino:", "Destino:");
+                    MenuInput m7 = new MenuInput("Insira a capacidade:", "Capacidade:");
+                    m5.executa();
+                    m6.executa();
+                    boolean dummyflag;
+                    int n = 0;
+                    do {
+                        dummyflag = false;
+                        m7.executa();
+                        try {
+                            n = Integer.parseInt(m7.getOpcao());
+
+                        } catch (NumberFormatException e) {
+                            dummyflag = true;
+                        }
+                    } while (dummyflag);
+
+                    cliente.addVoo(m5.getOpcao(), m6.getOpcao(), n);
+                    break;
+                    /*
+                case 4:
+                    //Menu de datas
+                    LocalDate l = LocalDate.now();
+                    t = new Cliente.Thread4(l);
+                    break;
+                case 5:
+                    //Menus de origem, destino, e data inicial e final
+                    String origem = "";
+                    List<String> dest = new ArrayList<>();
+                    LocalDate inDate = LocalDate.now(), finDate = LocalDate.now();
+
+                    t = new Cliente.Thread5(origem, dest, inDate, finDate);
+                    break;
+                case 6:
+                    t = new Cliente.Thread6();
+                    break;
+                case 7:
+                    t = new Cliente.Thread7();
+                    break;
+                case 8:
+                    MenuInput m8 = new MenuInput("Insira o codigo de reserva:", "Cod Reserva:");
+                    t = new Cliente.Thread8(m8.getOpcao());
+                    break;
+                case 9:
+                    t = new Cliente.Thread9();
+                    break;
+                default:
+                    flag = true;
+                    break;
+
+                     */
+            }
+            //if (!flag) t.start();
+        }
+    }
+}
